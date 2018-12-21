000507b7a907f33fee655c3b4835f4312a8d11a1
Ruter <ruter.lu@gmail.com>
2018-12-21 14:13:47 +0800

    [12.0][MIG] web_form_dynamic_button (#12)
    * [MIG] web_form_dynamic_button
    
    * [UPD] Update .gitignore

---
+++ web_form_dynamic_button/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hide buttons in form views",
+    'summary': "Allows you to dynamically hide buttons on form views",
+
+    'author': "深圳市知链科技有限公司",
+    'website': "https://github.com/cognichain/odoo-basic-extension",
+
+    'category': 'Tools',
+    'version': '11.0.1.0.1',
+    'license': 'LGPL-3',
+
+    'depends': ['web'],
+
+    'data': ['views/web_form_dynamic_button.xml'],
+    'demo': [],
+}
