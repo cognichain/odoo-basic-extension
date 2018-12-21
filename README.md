b08bf8019b90154276b8d6d6fcdf8632a41de6e9
Ruter <ruter@users.noreply.github.com>
2018-12-21 11:34:51 +0800

    [12.0][MIG] web_widget_many2many_tags_open (#8)
    * [UPD] Update README.md
    
    * [MIG] web_widget_many2many_tags_open

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# odoo-basic-extension
+![11.0](https://img.shields.io/badge/12.0-success-brightgreen.svg)
+[![License](https://img.shields.io/badge/license-LGPL--3.0-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0-standalone.html)
+
+# Basic extensions for Odoo
+
 The basic extensions for Odoo ✨
---
+++ web_widget_many2many_tags_open/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open many2many_tags record in form views",
+    'summary': "Make many2many_tags clickable on form views",
+
+    'author': "深圳市知链科技有限公司",
+    'website': "https://github.com/cognichain/odoo-basic-extension",
+
+    'category': 'Tools',
+    'version': '11.0.1.0.1',
+    'license': 'LGPL-3',
+
+    'depends': ['web'],
+
+    'data': ['views/web_widget_many2many_tags_open.xml'],
+    'demo': [],
+}
