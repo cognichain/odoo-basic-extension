626066cd4f18d01c3c5ee550ae54e9d7fbcdde1f
Ruter <ruter.lv@gmail.com>
2019-01-09 11:59:20 +0800

    [ADD] user_log module

---
+++ user_log/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "User Log",
+
+    'summary': "记录用户登录时的元信息",
+
+    'author': "深圳市知链科技有限公司",
+    'website': "https://github.com/cognichain/odoo-basic-extension",
+
+    'category': 'Tools',
+    'version': '11.0.1.0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/res_users_view.xml',
+    ]
+}
