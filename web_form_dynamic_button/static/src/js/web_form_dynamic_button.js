39f9edc82252269e2c172cc1562108b8209f45f3
Ruter <ruter.lv@gmail.com>
2019-01-29 11:57:25 +0800

    [FIX] this extension will remove customize server action buttons

--- web_form_dynamic_button/static/src/js/web_form_dynamic_button.js
+++ web_form_dynamic_button/static/src/js/web_form_dynamic_button.js
@@ -44,15 +44,21 @@ odoo.define('web_form_dynamic_button', function (require) {
                 _.each(actions, function (action) {
                     var expr = attrs[action + '_expr'];
                     var act_res = expr ? self._evalActionExpr(expr) : self.activeActions[action];
+                    var capAct = _.string.capitalize(action);
+                    var t_label = _t(capAct);
                     if (act_res) {
-                        var capAct = _.string.capitalize(action);
                         otherItems.push({
-                            label: _t(capAct),
+                            label: t_label,
                             callback: self['_on' + capAct + 'Record'].bind(self),
                         });
                     }
+                    // 删除 delete 和 duplicate 动作
+                    self.sidebar.items.other = self.sidebar.items.other.filter(function (item) {
+                        return item.label != t_label;
+                    });
                 });
-                this.sidebar.items.other = otherItems;
+                // 若条件成立，将动作添加到 other 中
+                this.sidebar.items.other = otherItems.concat(this.sidebar.items.other);
                 this._updateEnv();
             }
         },
